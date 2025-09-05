1374e2f23ed871316101836ad063c5f9cc1d6151
romaulat <112098158+romaulat@users.noreply.github.com>
2025-09-05 11:08:22 +0800

    Add files via upload

---
+++ config/apikeys.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'keys' => env('GOOGLE_API_KEY', ''),
+];
---
+++ config/paypal.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'client_id' => 'AZqjwiZe6Q31dqfNu3qQv8goE59I_qZUk3Dv7OOD4li7Au45jyo573HSC8LojraLZmVkA2UP3hxcyjDb',
+    'secret' => 'EID_RwGQWnE0BTHEIQc51mN_k0Zw8SreEOqMB6kmgnEaFTbcOA5htQntFVoAE8Gp75q_-hEER-zd7uhE',
+    'settings' => array(
+        'mode' => 'sandbox',
+        'http.ConnectionTimeOut' => 1000,
+        'log.LogEnabled' => true,
+        'log.FileName' => storage_path() . '/logs/paypal.log',
+        'log.LogLevel' => 'FINE'
+    ),
+
+];
